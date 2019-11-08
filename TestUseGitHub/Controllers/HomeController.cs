6281a14d316f6a4a0fc33068343dbf3f97207ddc
TP042319 <TP042319@TL06-03-24>
2019-11-08 15:37:28 +0800

    Change Back

--- TestUseGitHub/Controllers/HomeController.cs
+++ TestUseGitHub/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace TestUseGitHub.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page. Checking";
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
