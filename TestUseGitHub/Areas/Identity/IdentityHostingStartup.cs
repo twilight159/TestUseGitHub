02d98de925cad630d8b5a2076ebf8680b2e0fd26
TP042319 <TP042319@TL06-03-24>
2019-11-08 16:15:38 +0800

    Login

---
+++ TestUseGitHub/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestUseGitHub.Models;
+
+[assembly: HostingStartup(typeof(TestUseGitHub.Areas.Identity.IdentityHostingStartup))]
+namespace TestUseGitHub.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TestUseGitHubContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TestUseGitHubContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<TestUseGitHubContext>();
+            });
+        }
+    }
+}
